6edf016e1151f166740ccb7ce64317b6489fc9b4
wKich <nflazzer@gmail.com>
2015-09-20 13:10:08 +0500

    Add google maps api.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,39 @@
 //vehicle = [150, 376, 591, 777, 807];
 
 var socket = io('http://localhost:3000');
-  socket.on('route', function (data) {
-    console.log(data);
+socket.on('route', console.log.bind(console));
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
   });
+
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.POLYGON,
+        google.maps.drawing.OverlayType.POLYLINE,
+        google.maps.drawing.OverlayType.RECTANGLE
+      ]
+    },
+    markerOptions: {icon: 'images/beachflag.png'},
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+  drawingManager.setMap(map);
+}
 
 //setTimeout(function () {
     //var req = new XMLHttpRequest();
