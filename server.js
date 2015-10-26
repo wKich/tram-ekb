a79ceec1ab4ff6e8958ebb04a60f442c6bc34bbf
wKich <nflazzer@gmail.com>
2015-10-26 10:49:26 +0500

    Refactor indentation

--- server.js
+++ server.js
@@ -11,54 +11,54 @@ const routesList = Array.from({length: 34}, (v, k) => k+1)
 let tram = Tram(routesList)
 
 io.on('connection', (socket) => {
-    let routes = []
-    let trams = []
-    socket.on('startRoute', async function (number) {
-        if (number < 1 && number > 34)
-            return socket.emit('error', 'Route number out of range [1, 34]')
+  let routes = []
+  let trams = []
+  socket.on('startRoute', async function (number) {
+    if (number < 1 && number > 34)
+      return socket.emit('error', 'Route number out of range [1, 34]')
 
-        if (routes.indexOf(number) != -1)
-            return socket.emit('error', 'Route is already listening')
+    if (routes.indexOf(number) != -1)
+      return socket.emit('error', 'Route is already listening')
 
-        routes.push(number)
-    })
-    socket.on('stopRoute', async function (number) {
-        let index = routes.indexOf(number)
-        if (index == -1)
-            return socket.emit('error', 'Don\'t have that route number')
+    routes.push(number)
+  })
+  socket.on('stopRoute', async function (number) {
+    let index = routes.indexOf(number)
+    if (index == -1)
+      return socket.emit('error', 'Don\'t have that route number')
 
-        routes.splice(index, 1)
-    })
+    routes.splice(index, 1)
+  })
 
-    socket.on('startNumber', async function (number) {
-      if (number < 1 && number > 999)
-        return socket.emit('error', 'Tram number sould be in range [1, 999]')
+  socket.on('startNumber', async function (number) {
+    if (number < 1 && number > 999)
+      return socket.emit('error', 'Tram number sould be in range [1, 999]')
 
-      if (trams.indexOf(number) != -1)
-        return socket.emit('error', 'Tram is already listening')
+    if (trams.indexOf(number) != -1)
+      return socket.emit('error', 'Tram is already listening')
 
-      trams.push(number)
-    })
+    trams.push(number)
+  })
 
-    socket.on('stopNumber', async function (number) {
-      let index = trams.indexOf(number)
-      if (index == -1)
-        return socket.emit('error', 'Don\'t have that tram number')
+  socket.on('stopNumber', async function (number) {
+    let index = trams.indexOf(number)
+    if (index == -1)
+      return socket.emit('error', 'Don\'t have that tram number')
 
-      trams.splice(index, 1)
-    })
+    trams.splice(index, 1)
+  })
 
-    socket.on('routeCoordinates', async function (number) {
-      if (number < 1 && number > 34)
-        return socket.emit('error', 'Route number out of range [1, 34]')
+  socket.on('routeCoordinates', async function (number) {
+    if (number < 1 && number > 34)
+      return socket.emit('error', 'Route number out of range [1, 34]')
 
-      socket.emit('routeCoordinates', {number, path: await tram.getRouteCoordinates(number)})
-    })
+    socket.emit('routeCoordinates', {number, path: await tram.getRouteCoordinates(number)})
+  })
 
-    setInterval(() => {
-        socket.emit('route', tram.getTramsByRoutes(routes))
-        socket.emit('number', tram.getTramsByNumbers(trams))
-    }, 5000)
+  setInterval(() => {
+    socket.emit('route', tram.getTramsByRoutes(routes))
+    socket.emit('number', tram.getTramsByNumbers(trams))
+  }, 5000)
 })
 
 app.use(express.static('public'))
--- tram.js
+++ tram.js
@@ -21,171 +21,170 @@ import { MongoClient } from 'mongodb'
 //routeNums 1..34
 
 export default (routes) => {
-    //get cookie
-    const hostname = 'edu-ekb.ru'
-    let routesCollection
-    let dataCollection
-    let trams = []
-    let cookie
-    let headers = {}
+  const hostname = 'edu-ekb.ru'
+  let routesCollection
+  let dataCollection
+  let trams = []
+  let cookie
+  let headers = {}
 
-    console.log(` -> Connecting to mongoDB`)
-    MongoClient.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`)
-      .then(async function (db) {
-        routesCollection = db.collection('routes')
-        dataCollection = db.collection('data')
+  console.log(` -> Connecting to mongoDB`)
+  MongoClient.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`)
+    .then(async function (db) {
+      routesCollection = db.collection('routes')
+      dataCollection = db.collection('data')
 
-        console.log(' -> Check cookie')
-        if (!cookie) {
-            await _getCookie()
-            console.log(' -> Cookie getted: %s', cookie)
-        }
-        try {
-          await _updateRoutesCoordinates()
-        } catch (err) { console.log(err.message) }
-    })
-      .catch((err) => console.log(` -> Failed to connect to mongoDB: ${err.message}`))
-
-    async function _httpRequest ({
-            path = '/',
-            method = 'GET'
-        }, callback)
-    {
-        let options = { hostname, path, method, headers }
-        console.log(` -> Making ${method} request http://${hostname}${path}`)
-        return new Promise((resolve, reject) => {
-            let req = http.request(options, (res) => callback(res, resolve))
-            req.on('error', reject)
-            req.write('')
-            req.end()
-        })
-    }
-
-    async function _getCookie () {
-        console.log(' -> Getting cookie')
-        //use find instead of index
-        cookie = await _httpRequest({path: '/gmap/', method: 'HEAD'}, (res, callback) => callback(res.headers['set-cookie'][0].split(';', 1)[0]))
-        headers = {
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'Cookie': cookie,
-            'Host': `www.${hostname}`,
-            'Referer': `http://www.${hostname}/gmap/`
-        }
-    }
-
-    async function _getRoute (routeNums) {
-        console.log(' -> Check cookie')
-        if (!cookie) {
-            await _getCookie()
-            console.log(' -> Cookie getted: %s', cookie)
-        }
-
-        console.log(' -> Getting route')
-        return _httpRequest({ path: `/gmap/resources/entities.vgeopoint/mar/,tram_${routeNums.join(',tram_')},`},
-          (res, callback) => {
-            let body = ''
-            res.setEncoding('utf8')
-            res.on('data', (chunk) => {
-                body += chunk
-            })
-            res.on('end', () => callback(body))
-        })
-    }
-
-    async function _getCoordinates (routeNum) {
       console.log(' -> Check cookie')
       if (!cookie) {
         await _getCookie()
         console.log(' -> Cookie getted: %s', cookie)
       }
+      try {
+        await _updateRoutesCoordinates()
+      } catch (err) { console.log(err.message) }
+  })
+    .catch((err) => console.log(` -> Failed to connect to mongoDB: ${err.message}`))
 
-      console.log(' -> Getting route')
-      return _httpRequest({ path: `/gmap/resources/entities.marshlatlong/names/${routeNum}/tram`},
-        (res, callback) => {
-          let body = ''
-          res.setEncoding('utf8')
-          res.on('data', (chunk) => {
-            body += chunk
-          })
-          res.on('end', () => callback(body))
-      })
+  async function _httpRequest ({
+      path = '/',
+      method = 'GET'
+    }, callback)
+  {
+    let options = { hostname, path, method, headers }
+    console.log(` -> Making ${method} request http://${hostname}${path}`)
+    return new Promise((resolve, reject) => {
+      let req = http.request(options, (res) => callback(res, resolve))
+      req.on('error', reject)
+      req.write('')
+      req.end()
+    })
+  }
+
+  async function _getCookie () {
+    console.log(' -> Getting cookie')
+    //use find instead of index
+    cookie = await _httpRequest({path: '/gmap/', method: 'HEAD'}, (res, callback) => callback(res.headers['set-cookie'][0].split(';', 1)[0]))
+    headers = {
+      'Accept': 'application/json, text/javascript, */*; q=0.01',
+      'Cookie': cookie,
+      'Host': `www.${hostname}`,
+      'Referer': `http://www.${hostname}/gmap/`
+    }
+  }
+
+  async function _getRoute (routeNums) {
+    console.log(' -> Check cookie')
+    if (!cookie) {
+      await _getCookie()
+      console.log(' -> Cookie getted: %s', cookie)
     }
 
-    async function _updateRoutesCoordinates () {
-      console.log(` -> Updating routes coordinates`)
-      let routesCoordinates = (
-        await Promise.all(
-          routes.map(_getCoordinates)
-        ))
-        .map(JSON.parse)
-        .reduce((p, v, i) => {
-          p[i + 1] = v
-          return p
-        }, {})
-      let dbRoutes = (await routesCollection.find({}).toArray()).reduce((p, v) => { p[v.route] = v.coordinates; return p; }, {})
-      for (let route in routesCoordinates) {
-        console.log(` -> Compare route coordinates for route ${route}`)
-        if (!dbRoutes[route] ||
-            !dbRoutes[route].every((coordinate, index) => {
-              let flag = true
-              for (let key in coordinate) {
-                if (routesCoordinates[route][key] != coordinate[key])
-                  flag = false
-              }
-              return flag
-            }))
-          await routesCollection.updateOne({ route }, { $set: { coordinates: routesCoordinates[route] } }, { upsert: true })
-      }
+    console.log(' -> Getting route')
+    return _httpRequest({ path: `/gmap/resources/entities.vgeopoint/mar/,tram_${routeNums.join(',tram_')},`},
+      (res, callback) => {
+        let body = ''
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => {
+          body += chunk
+        })
+        res.on('end', () => callback(body))
+    })
+  }
+
+  async function _getCoordinates (routeNum) {
+    console.log(' -> Check cookie')
+    if (!cookie) {
+      await _getCookie()
+      console.log(' -> Cookie getted: %s', cookie)
     }
 
-    let getTramsByRoutes = (routeNumbers) => {
-        let resultTrams = []
-        routeNumbers.forEach((route) => {
-            let index = routes.indexOf(route)
-            if (index != -1) {
-                resultTrams.push(trams[index])
+    console.log(' -> Getting route')
+    return _httpRequest({ path: `/gmap/resources/entities.marshlatlong/names/${routeNum}/tram`},
+      (res, callback) => {
+        let body = ''
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => {
+          body += chunk
+        })
+        res.on('end', () => callback(body))
+    })
+  }
+
+  async function _updateRoutesCoordinates () {
+    console.log(` -> Updating routes coordinates`)
+    let routesCoordinates = (
+      await Promise.all(
+        routes.map(_getCoordinates)
+      ))
+      .map(JSON.parse)
+      .reduce((p, v, i) => {
+        p[i + 1] = v
+        return p
+      }, {})
+    let dbRoutes = (await routesCollection.find({}).toArray()).reduce((p, v) => { p[v.route] = v.coordinates; return p; }, {})
+    for (let route in routesCoordinates) {
+      console.log(` -> Compare route coordinates for route ${route}`)
+      if (!dbRoutes[route] ||
+          !dbRoutes[route].every((coordinate, index) => {
+            let flag = true
+            for (let key in coordinate) {
+              if (routesCoordinates[route][key] != coordinate[key])
+                flag = false
             }
-        })
-        return resultTrams
+            return flag
+          }))
+        await routesCollection.updateOne({ route }, { $set: { coordinates: routesCoordinates[route] } }, { upsert: true })
     }
+  }
 
-    let getTramsByNumbers = (tramNumbers) => {
-      let resultTrams = []
-      tramNumbers.forEach((number) => {
-        let tram
-        trams.forEach((route) => {
-          route.forEach((t) => { if (t.vehicle == number) tram = t })
-        })
-        if (tram)
-          resultTrams.push(tram)
+  let getTramsByRoutes = (routeNumbers) => {
+    let resultTrams = []
+    routeNumbers.forEach((route) => {
+      let index = routes.indexOf(route)
+      if (index != -1) {
+        resultTrams.push(trams[index])
+      }
+    })
+    return resultTrams
+  }
+
+  let getTramsByNumbers = (tramNumbers) => {
+    let resultTrams = []
+    tramNumbers.forEach((number) => {
+      let tram
+      trams.forEach((route) => {
+        route.forEach((t) => { if (t.vehicle == number) tram = t })
       })
-      return resultTrams
-    }
+      if (tram)
+        resultTrams.push(tram)
+    })
+    return resultTrams
+  }
 
-    let getRouteCoordinates = (routeNumber) => {
-      return new Promise((resolve, reject) => {
-        if (routesCollection)
-          routesCollection.find({ route: routeNumber }).limit(1).next().then(({coordinates}) => resolve(coordinates))
-        else
-          reject(new Error(`DB is unavailable`))
+  let getRouteCoordinates = (routeNumber) => {
+    return new Promise((resolve, reject) => {
+      if (routesCollection)
+        routesCollection.find({ route: routeNumber }).limit(1).next().then(({coordinates}) => resolve(coordinates))
+      else
+        reject(new Error(`DB is unavailable`))
+    })
+  }
+
+  setInterval(async function () {
+    trams = JSON.parse(await _getRoute(routes))
+      .map(({latitude, longitude, marshrut, vehicle}) => {
+        let number = marshrut.slice(5)
+        console.log(` -> Route #${number} with tram #${vehicle} getted`)
+        return {number, latitude, longitude, vehicle}
       })
-    }
+      .reduce((p, v) => { p[v.number-1].push(v); return p; }, Array.from(routes, () => []))
+  }, 10000)
 
-    setInterval(async function () {
-      trams = JSON.parse(await _getRoute(routes))
-        .map(({latitude, longitude, marshrut, vehicle}) => {
-          let number = marshrut.slice(5)
-          console.log(` -> Route #${number} with tram #${vehicle} getted`)
-          return {number, latitude, longitude, vehicle}
-        })
-        .reduce((p, v) => { p[v.number-1].push(v); return p; }, Array.from(routes, () => []))
-    }, 10000)
+  setInterval(_updateRoutesCoordinates, 1000*60*60*24)
 
-    setInterval(_updateRoutesCoordinates, 1000*60*60*24)
-
-    return {
-        getTramsByRoutes,
-        getTramsByNumbers,
-        getRouteCoordinates
-    }
+  return {
+    getTramsByRoutes,
+    getTramsByNumbers,
+    getRouteCoordinates
+  }
 }
